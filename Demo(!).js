055dfb3bb2af2adc812d98e6dacbf987fc31caae
vinodhiniarji14-coder <vinodhiniarji14@gmail.com>
2026-03-24 11:16:06 +0530

    Create Demo(!).js

---
+++ Demo(!).js
@@ -0,0 +1,8 @@
+var message = "Hello TypeScript";
+var number1 = 10;
+var number2 = 20;
+function add(a, b) {
+    return a + b;
+}
+console.log(message);
+console.log("Sum:", add(number1, number2));
